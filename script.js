1c7c48653f51beb0a0e4f628aeded726b880a1b4
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-03-26 14:05:38 -0600

    finish project. cards now dispay once book is added. add remove feature

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const addBookButton = document.getElementById("addbookbutton");
 const formdiv = document.querySelector(".formdiv");
 const overlay = document.querySelector(".overlay");
-const readbutton = document.querySelector(".buttonread");
 const form = document.querySelector("#bookform");
 const maindiv = document.querySelector(".maindiv");
 const submitbutton = document.querySelector("#submitform");
 const formtitle = document.querySelector("#formtitle");
 const formauthor = document.querySelector("#formauthor");
 const formpages = document.querySelector("#formpages");
-const formisread = document.querySelector("#formisread");
 
 const myLibrary = [];
 
-function Book(name, author, pages, read) {
+function Book(name, author, pages) {
   this.name = name;
   this.author = author;
   this.pages = pages;
-  this.read = read;
 }
 
 function createCard(a) {
@@ -27,13 +24,39 @@ function createCard(a) {
   const pages = document.createElement("div");
   const readbutton = document.createElement("button");
   readbutton.classList.add("buttonread");
+
+  if (form.isread.checked === true) {
+    readbutton.classList.add("greenbutton");
+    readbutton.innerText = "Read";
+  } else {
+    readbutton.classList.add("redbutton");
+    readbutton.innerText = "Not read";
+  }
+
   const removebutton = document.createElement("button");
+  removebutton.innerText = "Remove";
   removebutton.classList.add("buttonremove");
   title.innerText = a.name;
   author.innerText = a.author;
   pages.innerText = a.pages;
   card.append(title, author, pages, readbutton, removebutton);
   maindiv.appendChild(card);
+  card.classList.add("cardactive");
+  readbutton.addEventListener("click", () => {
+    if (readbutton.classList.contains("greenbutton")) {
+      readbutton.classList.remove("greenbutton");
+      readbutton.classList.add("redbutton");
+      readbutton.innerText = "Not read";
+    } else {
+      readbutton.classList.remove("redbutton");
+      readbutton.classList.add("greenbutton");
+      readbutton.innerText = "Read";
+    }
+  });
+
+  removebutton.addEventListener("click", () => {
+    card.remove();
+  });
 }
 
 addBookButton.addEventListener("click", () => {
@@ -49,23 +72,14 @@ overlay.addEventListener("click", () => {
   }
 });
 
-readbutton.addEventListener("click", () => {
-  if (readbutton.classList.contains("greenbutton")) {
-    readbutton.classList.remove("greenbutton");
-    readbutton.classList.add("redbutton");
-    readbutton.innerText = "Not read";
-  } else {
-    readbutton.classList.remove("redbutton");
-    readbutton.classList.add("greenbutton");
-    readbutton.innerText = "Read";
-  }
-});
-
 submitbutton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const newbook = new Book(formtitle.value, formauthor.value, formpages.value, formisread.value);
+  if (formtitle.value === "" || formauthor.value === "" || formpages.value === "") return;
+  const newbook = new Book(formtitle.value, formauthor.value, formpages.value);
   myLibrary.push(newbook);
   createCard(myLibrary[myLibrary.length - 1]);
   overlay.classList.remove("active");
   formdiv.classList.remove("displayblock");
+  e.preventDefault();
 });
+
+// on next library. link the actual cards to the array. and create each card by looping through the array.
